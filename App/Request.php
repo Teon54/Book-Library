e4b16a14038e952bb447ec885f9bf5f4d496a3b3
Sajjad Mohammadi <mohammadisajjad54@gmail.com>
2023-11-18 14:22:43 +0330

    Creating a requset class to get commands and parameters

--- App/Request.php
+++ App/Request.php
@@ -4,5 +4,8 @@ namespace App;
 
 class Request
 {
-
+    public function __construct(string $commandFilePath, public ?\stdClass $request = null)
+    {
+        $this->request = json_decode(file_get_contents(__DIR__ . '/../' . $commandFilePath));
+    }
 }
--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 require_once 'vendor/autoload.php';
 
 use App\Request;
+
+print_r((new Request('commands.json')));
