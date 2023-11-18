4cc06f3613d767e9fad0d4a239541cfc46df29c7
Sajjad Mohammadi <mohammadisajjad54@gmail.com>
2023-11-18 21:10:57 +0330

    Create trait timestamp

---
+++ App/DTO/JsonFileReader.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\DTO;
+
+class JsonFileReader
+{
+
+    public function __construct()
+    {
+    }
+}
---
+++ App/TimeStampTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use DateTime;
+
+trait TimeStampTrait
+{
+    public function getTimeStampedArray($booksData): array
+    {
+        foreach ($booksData as $book) {
+            if ($book->publishDate instanceof DateTime) {
+                continue;
+            }
+            $book->publishDate = (new DateTime())->setTimestamp((strtotime($book->publishDate)));
+        }
+        return $booksData;
+    }
+}
