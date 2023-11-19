e66868cb9660113512dfe16349bcfc597927fb7d
Sajjad Mohammadi <mohammadisajjad54@gmail.com>
2023-11-19 17:31:49 +0330

    fix pagination books page number

--- App/PaginationBooks.php
+++ App/PaginationBooks.php
@@ -5,14 +5,19 @@ namespace App;
 class PaginationBooks
 {
 
-    public function getPaginatedBooks(array $booksData,Request $request): ?array{
+    public function getPaginatedBooks(array $booksData, Request $request): ?array
+    {
         $arrayRequest = get_object_vars($request->request->parameters);
         $arrayRequest = array_map(function ($value) {
             return is_string($value) ? strtolower($value) : $value;
         }
             , $arrayRequest);
         $arrayRequest = array_change_key_case($arrayRequest);
-        $paginatedBooksData = array_chunk($booksData,$arrayRequest['perpage']);
-        return $arrayRequest['page'] > count($paginatedBooksData) ? null : $paginatedBooksData[$arrayRequest['page']];
+        $paginatedBooksData = array_chunk($booksData, $arrayRequest['perpage']);
+        if ($arrayRequest['page'] - 1 >= count($paginatedBooksData)) {
+            return null;
+        } else {
+            return $paginatedBooksData[$arrayRequest['page'] - 1];
+        }
     }
 }
