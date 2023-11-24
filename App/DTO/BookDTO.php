53d74fc368ccfac028b5188cc1062cd2bc0b48d0
Sajjad Mohammadi <mohammadisajjad54@gmail.com>
2023-11-24 17:07:19 +0330

    fix BookDTO.php file now DTO is object for each book

--- App/DTO/BookDTO.php
+++ App/DTO/BookDTO.php
@@ -4,13 +4,12 @@ namespace App\DTO;
 
 class BookDTO
 {
-    public function __construct(public array $bookData)
-    {
-    }
-
-    public function add(BookDTO $booksData): BookDTO
-    {
-        array_push($this->bookData, ...$booksData->bookData);
-        return $this;
+    public function __construct(
+        public string $ISBN,
+        public string $bookTitle,
+        public string $authorName,
+        public int $pagesCount,
+        public string $publishDate
+    ) {
     }
 }
